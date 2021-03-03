ed3a0f3a249516b9b7275f9848ceb80c2f15d19f
Leonor <leonorpatriciasf@hotmail.com>
2021-03-03 12:30:50 +0000

    Valores Especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"int max = {int.MaxValue}");
+            Console.WriteLine($"int min = {int.MinValue}");
+            Console.WriteLine($"double max = {double.MaxValue}");
+            Console.WriteLine($"double min = {double.MinValue}");
+            Console.WriteLine($"byte max = {byte.MaxValue}");
+            Console.WriteLine($"byte min = {byte.MinValue}");
+
+            Console.WriteLine($"double +inf = {double.PositiveInfinity}");
+            Console.WriteLine($"double -inf = {double.NegativeInfinity}");
+            Console.WriteLine($"double min = {double.NaN}");
+        }
+    }
+}
